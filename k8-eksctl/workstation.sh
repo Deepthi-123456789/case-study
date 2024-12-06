e6f9e4c9f1d5d8bb05d59f6dd9db89e1aab2d39b
Deepthi <jonnalagaddadeepthi96@gmail.com>
2024-12-06 19:19:01 +0530

    first

--- k8-eksctl/workstation.sh
+++ k8-eksctl/workstation.sh
@@ -8,7 +8,6 @@ N="\e[0m"
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
-
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -32,45 +31,60 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
+# Installing yum-utils
 yum install -y yum-utils
-VALIDATE $? "Installed yum utils"
+VALIDATE $? "Installed yum-utils"
 
+# Adding Docker repository
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 VALIDATE $? "Added docker repo"
 
+# Installing Docker components
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 VALIDATE $? "Installed docker components"
 
+# Starting and enabling Docker
 systemctl start docker
 VALIDATE $? "Started docker"
 
 systemctl enable docker
 VALIDATE $? "Enabled docker"
 
+# Adding centos user to docker group
 usermod -aG docker centos
-VALIDATE $? "added centos user to docker group"
+VALIDATE $? "Added centos user to docker group"
 echo -e "$R Logout and login again $N"
 
+# Installing kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "Kubectl installation"
 
+# Installing eksctl
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo mv /tmp/eksctl /usr/local/bin
 VALIDATE $? "eksctl installation"
 
+# Installing kubens
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 VALIDATE $? "kubens installation"
 
+# Installing Helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
-./get_helm.s
+./get_helm.sh
 
+# Adding Helm repo for AWS EBS CSI Driver
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
 helm repo update
+
+# Installing AWS EBS CSI Driver via Helm
 helm upgrade --install aws-ebs-csi-driver \
     --namespace kube-system \
     aws-ebs-csi-driver/aws-ebs-csi-driver
+VALIDATE $? "AWS EBS CSI Driver installation"
+
+echo -e "$G All required components have been installed successfully! $N"
